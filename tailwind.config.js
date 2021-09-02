e2dda310bd8ac635caf4e923283610e634e69f09
Claire Botman <marshy@zeta.com.au>
2021-09-02 08:09:19 +0800

    tailwindcss jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	mode: 'jit',
+	purge: [
+		'./src/*.{js,jsx,ts,tsx}',
+		'./src/**/*.{js,jsx,ts,tsx}',
+		'./src/**/**/*.{js,jsx,ts,tsx}',
+		'./public/index.html',
+	],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {},
