38314d176e3dfd42f02269f54a29af4c868c96b0
Claire Botman <marshy@zeta.com.au>
2021-09-05 07:41:52 +0800

    prettify success alert

--- src/components/alerts/Success.js
+++ src/components/alerts/Success.js
@@ -1,6 +1,8 @@
 export default function Success(props) {
 	return (
-		<div className="rounded-md bg-green-50 p-4">
+		<div
+			className={`rounded-md bg-green-50  border border-green-100 p-4  ${props.className}`}
+		>
 			<div className="flex">
 				<p className="text-sm font-medium text-green-800">
 					{props.message}
