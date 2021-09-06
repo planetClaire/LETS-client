48e832fba59a47ff150f2ee53806a36ab9c1c815
Claire Botman <marshy@zeta.com.au>
2021-09-06 10:38:40 +0800

    wait for auth to initialise before redirecting

--- src/components/auth/MemberRoute.js
+++ src/components/auth/MemberRoute.js
@@ -1,28 +1,36 @@
+import { useEffect, useState } from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
 import { useAuth } from './useAuth';
 
 export default function MemberRoute({ component: Component, ...rest }) {
 	let auth = useAuth();
-
+	let [user, setUser] = useState();
+	useEffect(() => {
+		if (auth.user !== undefined && auth.user !== null) {
+			setUser(auth.user);
+		}
+	}, [auth]);
 	return (
-		<Route
-			{...rest}
-			render={({ location }) => {
-				if (auth.user === false) {
-					return (
-						<Redirect
-							to={{
-								pathname: '/login',
-								state: { pathname: location.pathname },
-							}}
-						/>
-					);
-				} else if (auth.user && !auth.user.emailVerified) {
-					return <Redirect to={{ pathname: '/verifyEmail' }} />;
-				}
-				return <Component />;
-			}}
-		/>
+		user !== undefined && (
+			<Route
+				{...rest}
+				render={({ location }) => {
+					if (user === false) {
+						return (
+							<Redirect
+								to={{
+									pathname: '/login',
+									state: { pathname: location.pathname },
+								}}
+							/>
+						);
+					} else if (!user.emailVerified) {
+						return <Redirect to={{ pathname: '/verifyEmail' }} />;
+					}
+					return <Component />;
+				}}
+			/>
+		)
 	);
 }
